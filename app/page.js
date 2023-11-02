76ef750f06fe99381032d7f05039345f576dbcfb
huy.duong <huy.duong@mainbridgehp.com>
2023-11-02 23:19:08 +0700

    repair format

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ BEGIN
   PRINT 'Creating SP [dbo].[${nameStore}]'
 END
 GO
-    ${sql}
+${sql}
 GO
 PRINT 'Altering SP [dbo].[${nameStore}]'
 `
@@ -34,8 +34,6 @@ PRINT 'Altering SP [dbo].[${nameStore}]'
             <div>
                     <input
                         type="text"
-                        name="price"
-                        id="price"
                         className="block h-12 w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Name store"
                         onChange={(e) =>handNameStore(e)}
