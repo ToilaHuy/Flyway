f63d6a1fa1d9cbcfc0e49278413125d7c4549f96
huy.duong <huy.duong@mainbridgehp.com>
2023-11-10 15:48:07 +0700

    v3

--- app/page.js
+++ app/page.js
@@ -1,51 +1,70 @@
 "use client"
 import Image from 'next/image'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Form, useFieldArray, useForm} from "react-hook-form";
 
 export default function Home() {
     const [nameStore,setNameStore] = useState()
-    const [sql,setSql] = useState()
+    const [sql,setSql] = useState("")
     const [generateSql, setGenerateSql] = useState("")
     const { register, control, handleSubmit } = useForm({
         defaultValues: {
-            test:[{},{}],
+            table:[{},{}],
+            store:[{}]
         },
+
         // defaultValues: {}; you can populate the fields by this attribute
     });
-    const { fields, append, remove } = useFieldArray({
+    const { fields:fieldsTable, append:appendTable, remove:removeTable } = useFieldArray({
         control,
-        name: "test"
+        name: "table"
     });
+    const { fields:fieldsStore, append:appendStore, remove:removeStore } = useFieldArray({
+        control,
+        name: "store"
+    });
+
     const handNameStore = (e) => {
         setNameStore(e.target.value)
     }
     const handInputSql = (e) => {
         setSql(e.target.value)
     }
-    const handGenerateSql = () => {
-const text =
-`
---${nameStore}
-IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[${nameStore}]'))
+    useEffect(() => {
+        setSql(prevState => prevState = generateSql)
+    },[generateSql])
+    const handGenerateSql = data => {
+        console.log(data)
+        setGenerateSql("")
+        let text = ""
+        data.store.map(item => {
+            if(item.name.length > 0){
+                text +=
+                    `
+--${item.name}
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[${item.name}]'))
 BEGIN
-  EXEC sp_executesql N'CREATE PROCEDURE [dbo].[${nameStore}] AS select 1'
-  PRINT 'Creating SP [dbo].[${nameStore}]'
+  EXEC sp_executesql N'CREATE PROCEDURE [dbo].[${item.name}] AS select 1'
+  PRINT 'Creating SP [dbo].[${item.name}]'
 END
 GO
-${sql}
+${item.value}
 GO
-PRINT 'Altering SP [dbo].[${nameStore}]'
+PRINT 'Altering SP [dbo].[${item.name}]'
 `
-     setGenerateSql(text)
+            }
+        })
+     setGenerateSql( prevState => prevState = text)
+        console.log(generateSql)
     }
     const onFormSubmit = data => {
         let text = "";
         const regex = /\[(.*?)\]/;
 
-    data.test.map(item => {
+    data.table.map(item => {
         const matches = item.value.match(regex);
         const nameColumn = matches ? matches[1] : null;
+        setGenerateSql("")
         if(nameColumn !== null){
             text += `
 IF NOT EXISTS (
@@ -71,44 +90,49 @@ END
     }
   return (
     <main className=" min-h-screenh items-center p-24">
-        <div className="flex gap-4 items-center mb-3">
-            <div>
+        <div className="flex-col  gap-4 items-center">
+           <div>
+               <form onSubmit={handleSubmit(handGenerateSql)}>
+                   <h1>store</h1>
+                   <ul>
+                       {fieldsStore.map((item, index) => (
+                           <li key={item.id} className="flex gap-4 mb-3">
+                               <input className="content-stretch max-w-3xl rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
+                                         type="text" {...register(`store.${index}.name`)} />
+                               <textarea className="content-stretch w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
+                                      {...register(`store.${index}.value`)} />
+                               <button type="button" className="bg-sky-500 h-12 p-2 rounded-lg mr-3" onClick={() => removeStore( index)}>Delete</button>
+                           </li>
+                       ))}
+                   </ul>
+
+                   <div>
+                       <button className="bg-sky-500 h-12 p-2 rounded-lg mr-3"
+                               type="button"
+                               onClick={() => appendStore()}
+                       >
+                           append
+                       </button>
+                       <input className="bg-sky-500 h-12 p-2 rounded-lg" type="submit" />
+                   </div>
+               </form>
+           </div>
+            <form className="mt-6" onSubmit={handleSubmit(onFormSubmit)}>
+                <h1>Table</h1>
+                <div>
                     <input
                         type="text"
-                        className="block h-12 w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        placeholder="Name store"
+                        className="block h-12 max-w-3xl mb-3 rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Name table"
                         onChange={(e) =>handNameStore(e)}
                     />
-
-            </div>
-
-        </div>
-        <div className="flex  gap-4 items-center">
-
-           <div>
-               <h1>Store</h1>
-               <div>Input here</div>
-            <textarea
-                name="price"
-                id="price"
-                className="content-stretch w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 min-w-full"
-                onChange={(e) =>handInputSql(e)}
-            />
-               <button className="bg-sky-500 h-12 p-2 rounded-lg" onClick={() =>handGenerateSql()}>Generate</button>
-           </div>
-            <form onSubmit={handleSubmit(onFormSubmit)}>
-                <h1>Table</h1>
+                </div>
                 <ul>
-                    {fields.map((item, index) => (
+                    {fieldsTable.map((item, index) => (
                         <li key={item.id} className="flex gap-4 mb-3">
                             <input className="content-stretch w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 min-w-full"
-                                   type="text" {...register(`test.${index}.value`)} />
-                            {/*<Controller*/}
-                            {/*    render={({ field }) => <input {...field} />}*/}
-                            {/*    name={`test.${index}.lastName`}*/}
-                            {/*    control={control}*/}
-                            {/*/>*/}
-                            <button type="button" className="bg-sky-500 h-12 p-2 rounded-lg mr-3" onClick={() => remove(index)}>Delete</button>
+                                   type="text" {...register(`table.${index}.value`)} />
+                            <button type="button" className="bg-sky-500 h-12 p-2 rounded-lg mr-3" onClick={() => removeTable(index)}>Delete</button>
                         </li>
                     ))}
                 </ul>
@@ -116,7 +140,7 @@ END
                 <div>
                     <button className="bg-sky-500 h-12 p-2 rounded-lg mr-3"
                             type="button"
-                            onClick={() => append()}
+                            onClick={() => appendTable()}
                     >
                         append
                     </button>
@@ -126,9 +150,10 @@ END
         </div>
         <div>Copy here</div>
         <div className="h-96">
-            <textarea
+            <textarea    id="message"
+                         name="message"
                 className="h-full block w-full rounded-md border-0 py-1.5 pl-28 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={generateSql}
+                defaultValue={sql}
             />
         </div>
     </main>
